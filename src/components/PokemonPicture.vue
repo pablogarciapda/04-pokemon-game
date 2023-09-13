94b2bdaea5a89ceb6e83a22e392e4e5e21384994
Pablo García <pablogarciapda@hotmail.com>
2023-09-13 08:33:01 +0200

    esto esta con css

---
+++ src/components/PokemonPicture.vue
@@ -0,0 +1,46 @@
+<script setup>
+
+</script>
+
+<template>
+  <div class="pokemon-container">
+      <img class="hidden-pokemon" src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/1.svg" alt="pokemon ">
+      <img class="fade-in" src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/1.svg" alt="pokemon ">
+  </div>
+</template>
+
+<style scoped>
+/* Pokemon Picture */
+.pokemon-container {
+    height: 200px;
+}
+
+/* img {
+    height: 200px;
+    position: absolute;
+    right: 32%;
+    user-select: none;
+    -moz-user-select: none;
+    -ms-user-select: none;
+    -webkit-user-drag: none;
+    -webkit-user-select: none;
+} */
+
+img {
+    height: 200px;
+    position: absolute;
+    width: 100vw;
+    margin-left: -50vw;
+    user-select: none;
+    -moz-user-select: none;
+    -ms-user-select: none;
+    -webkit-user-drag: none;
+    -webkit-user-select: none;
+  }
+  
+.hidden-pokemon {
+    filter: brightness(0);
+}
+
+
+</style>
---
+++ src/pages/PokemonPage.vue
@@ -0,0 +1,17 @@
+<script setup>
+import PokemonOption from '../components/PokemonOption.vue'
+import PokemonPicture from '../components/PokemonPicture.vue'
+</script>
+
+<template>
+  <h1>Que pokemon es</h1>
+
+  <!-- todo imagen pokemon -->
+    <PokemonPicture />
+  <!-- todo Opciones pokemon -->
+  <PokemonOption />
+</template>
+
+<style scoped>
+
+</style>
